b140d85630838f79cb158321c6d2b7a3f76330c6
jpbonch <jp.bonchristiano@gmail.com>
2024-03-03 16:09:04 -0500

    Add reboot

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/quizbackend/deploy.log
 # nodejs-app is the same name as stored in pm2 process
-echo 'forever restart index.js' >> /home/ec2-user/quizbackend/deploy.log
-forever restart index.js >> /home/ec2-user/quizbackend/deploy.log
+echo 'reboot' >> /home/ec2-user/quizbackend/deploy.log
+reboot
