b3597cc2dedab1d2246dda4209c2b52394a48cf5
jpbonch <jp.bonchristiano@gmail.com>
2024-03-03 01:30:50 -0500

    Replaced stop with kill

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,8 +2,8 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/quizbackend/deploy.log
 # nodejs-app is the same name as stored in pm2 process
-echo 'pm2 stop index' >> /home/ec2-user/quizbackend/deploy.log
-sudo pm2 stop index >> /home/ec2-user/quizbackend/deploy.log
+echo 'pm2 kill' >> /home/ec2-user/quizbackend/deploy.log
+sudo pm2 kill >> /home/ec2-user/quizbackend/deploy.log
 
-echo 'pm2 start index' >> /home/ec2-user/quizbackend/deploy.log
-sudo pm2 start index >> /home/ec2-user/quizbackend/deploy.log
+echo 'pm2 start index.js' >> /home/ec2-user/quizbackend/deploy.log
+sudo pm2 start index.js >> /home/ec2-user/quizbackend/deploy.log
