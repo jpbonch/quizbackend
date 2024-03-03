634bedc8194fb6813c8af0a75e992718974eaf78
jpbonch <jp.bonchristiano@gmail.com>
2024-03-03 16:11:06 -0500

    Testing cicd speed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const MongoClient = require("mongodb");
 // test comment
 app.get("/", (req, res) => {
-  res.send("going bed");
+  res.send("testing changes");
 });
 
 app.get("/quizzes", async (req, res) => {
