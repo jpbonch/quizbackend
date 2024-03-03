a5532a471c862af13e50235d13573c0fde1dfadc
jpbonch <jp.bonchristiano@gmail.com>
2024-03-03 01:26:07 -0500

    Test CI/CD

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,8 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/quizbackend/deploy.log
 # nodejs-app is the same name as stored in pm2 process
-echo 'pm2 restart index' >> /home/ec2-user/quizbackend/deploy.log
-sudo pm2 restart index >> /home/ec2-user/quizbackend/deploy.log
+echo 'pm2 stop index' >> /home/ec2-user/quizbackend/deploy.log
+sudo pm2 stop index >> /home/ec2-user/quizbackend/deploy.log
+
+echo 'pm2 start index' >> /home/ec2-user/quizbackend/deploy.log
+sudo pm2 start index >> /home/ec2-user/quizbackend/deploy.log
