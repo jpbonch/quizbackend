dfc3c8f6a9242ffdc016cab719ef18b587f96359
jpbonch <jp.bonchristiano@gmail.com>
2024-03-02 19:29:48 -0500

    epic

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const port = 443;
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
