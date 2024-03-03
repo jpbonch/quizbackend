30575f00f51a287ce6afb40433cd89d36ebd0ff5
jpbonch <jp.bonchristiano@gmail.com>
2024-03-02 19:13:16 -0500

    Init

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = 443;
+import { MongoClient } from "mongodb";
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/quizzes", async (req, res) => {
+  const connectionString = process.env.ATLAS_URI || "";
+  const client = new MongoClient(connectionString);
+  let conn;
+  try {
+    conn = await client.connect();
+  } catch (e) {
+    console.error(e);
+  }
+  let db = conn.db("quizapp");
+  let collection = await db.collection("quizzes");
+  let results = await collection.find({}).limit(50).toArray();
+  res.send(results).status(200);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
