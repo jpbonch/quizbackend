7be1d1fd482478c4930b0fd676fc6b6bf54ba82f
jpbonch <jp.bonchristiano@gmail.com>
2024-03-03 00:51:33 -0500

    Modify script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/quizbackend/deploy.log
 # nodejs-app is the same name as stored in pm2 process
 echo 'pm2 restart index' >> /home/ec2-user/quizbackend/deploy.log
-pm2 restart index >> /home/ec2-user/quizbackend/deploy.log
+sudo pm2 restart index >> /home/ec2-user/quizbackend/deploy.log
