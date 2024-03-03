2d2b56d50d7f3ff37cdf5d7a1abffeebce85f8c3
jpbonch <jp.bonchristiano@gmail.com>
2024-03-03 00:39:38 -0500

    Fix scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/myrepo/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/quizbackend/deploy.log
 
-echo 'cd /home/ec2-user/myrepo' >> /home/ec2-user/myrepo/deploy.log
-cd /home/ec2-user/myrepo >> /home/ec2-user/myrepo/deploy.log
+echo 'cd /home/ec2-user/quizbackend' >> /home/ec2-user/quizbackend/deploy.log
+cd /home/ec2-user/quizbackend >> /home/ec2-user/quizbackend/deploy.log
 
-echo 'npm install' >> /home/ec2-user/myrepo/deploy.log 
-npm install >> /home/ec2-user/myrepo/deploy.log
+echo 'npm install' >> /home/ec2-user/quizbackend/deploy.log 
+npm install >> /home/ec2-user/quizbackend/deploy.log
