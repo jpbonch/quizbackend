317b23b0e1bc37bbc66b228de02e416eaf4d5886
jpbonch <jp.bonchristiano@gmail.com>
2024-03-03 02:20:55 -0500

    Minor fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const MongoClient = require("mongodb");
 // test comment
 app.get("/", (req, res) => {
-  res.send("hailmary");
+  res.send("small cahnge");
 });
 
 app.get("/quizzes", async (req, res) => {
