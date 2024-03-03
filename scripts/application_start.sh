32b3e774c181ce7772dcd998e1a409c6988dcfb2
jpbonch <jp.bonchristiano@gmail.com>
2024-03-03 01:41:48 -0500

    change

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,7 +3,7 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/quizbackend/deploy.log
 # nodejs-app is the same name as stored in pm2 process
 # echo 'pm2 kill' >> /home/ec2-user/quizbackend/deploy.log
-sudo pm2 delete all >> /home/ec2-user/quizbackend/deploy.log
+/usr/local/bin/pm2 delete all >> /home/ec2-user/quizbackend/deploy.log
 # echo 'deted' >> /home/ec2-user/quizbackend/deploy.log
 # echo 'pm2 start index.js' >> /home/ec2-user/quizbackend/deploy.log
 # sudo pm2 start index.js >> /home/ec2-user/quizbackend/deploy.log
