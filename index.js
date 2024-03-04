389c848f6ad321b1d838612f3ed675b426744c1f
jpbonch <jp.bonchristiano@gmail.com>
2024-03-04 17:51:38 -0500

    Update port

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
-const port = 4000;
 const mongoose = require('mongoose');
 const { Quiz, Question, User } = require("./models/models");
 
@@ -21,7 +20,7 @@ const start = async () => {
     await mongoose.connect(
       process.env.ATLAS_URI
     );
-    app.listen(3000, () => console.log("Server started on port 3000"));
+    app.listen(4000, () => console.log("Server started on port 4000"));
   } catch (error) {
     console.error(error);
     process.exit(1);
