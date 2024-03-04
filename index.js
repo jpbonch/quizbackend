484024bd1b7a361e18ee4cc50457b48025a66d0a
jpbonch <jp.bonchristiano@gmail.com>
2024-03-04 18:33:11 -0500

    Basic connection

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors")
 const app = express();
+app.use(cors())
 app.use(express.json());
 const http = require('http');
 const server = http.createServer(app);
-const port = 4000;
 const mongoose = require('mongoose');
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }
+});
+
 
 
 const { Quiz, Question, User } = require("./models/models");
@@ -19,6 +26,22 @@ app.get("/", (req, res) => {
 app.get("/quizzes", async (req, res) => {
   const allQuizzes = await Quiz.find();
   return res.status(200).json(allQuizzes);
+});
+
+let lastsocket;
+let playerCount = 0;
+let rooms = 0;
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  playerCount++;
+  if (playerCount == 2) {
+    socket.join("room" + rooms);
+    lastsocket.join("room" + rooms);
+    io.to("room" + rooms).emit("found");
+    rooms++;
+    playerCount = 0;
+  }
+  lastsocket = socket;
 });
 
 
